5a9e9dee00905f67662fef4cf0e07be1358d5bc6
DemonicSavage <gabrielhuwe@gmail.com>
2023-09-01 08:02:43 -0300

    Removed sentry and changed domain

--- src/reddit/compile.ts
+++ src/reddit/compile.ts
@@ -1,115 +1,133 @@
-import { HTMLElement } from 'node-html-parser';
-import { RedditPost } from './types';
-import { youtubeEmbed } from '../embeds/youtube';
-import { twitchClipEmbed } from '../embeds/twitch';
-import { twitterLinkEmbed } from '../embeds/twitter';
-import '../html';
+import { HTMLElement } from "node-html-parser";
+import { RedditPost } from "./types";
+import { youtubeEmbed } from "../embeds/youtube";
+import { twitchClipEmbed } from "../embeds/twitch";
+import { twitterLinkEmbed } from "../embeds/twitter";
+import "../html";
 
 function getDomainHandler(domain?: string) {
-    switch (domain) {
-        case 'youtu.be':
-        case 'www.youtube.com':
-        case 'youtube.com':
-            return {
-                handler: youtubeEmbed,
-                type: 'video.other',
-            };
-        case 'clips.twitch.tv':
-            return {
-                handler: twitchClipEmbed,
-                type: 'video.other',
-            };
-        case 'twitter.com':
-        case 'x.com':
-            return {
-                handler: twitterLinkEmbed,
-                type: 'summary',
-            };
-        default:
-            return null;
-    }
+  switch (domain) {
+    case "youtu.be":
+    case "www.youtube.com":
+    case "youtube.com":
+      return {
+        handler: youtubeEmbed,
+        type: "video.other",
+      };
+    case "clips.twitch.tv":
+      return {
+        handler: twitchClipEmbed,
+        type: "video.other",
+      };
+    case "twitter.com":
+    case "x.com":
+      return {
+        handler: twitterLinkEmbed,
+        type: "summary",
+      };
+    default:
+      return null;
+  }
 }
 
 export async function postToHtml(post: RedditPost): Promise<HTMLElement> {
-    const html = new HTMLElement('html', {});
-    const head = html.appendChild(new HTMLElement('head', {}));
+  const html = new HTMLElement("html", {});
+  const head = html.appendChild(new HTMLElement("head", {}));
 
-    head.meta('og:title', `r/${post.subreddit}: ${post.title}`);
-    head.meta('twitter:title', post.title);
-    head.meta('og:url', `https://www.reddit.com${post.permalink}`);
-    head.meta('og:site_name', 'rxddit.com');
-    head.meta('twitter:site', 'rxddit.com');
-    head.meta('theme-color', '#ff581a');
+  head.meta("og:title", `r/${post.subreddit}: ${post.title}`);
+  head.meta("twitter:title", post.title);
+  head.meta("og:url", `https://www.reddit.com${post.permalink}`);
+  head.meta("og:site_name", "***REMOVED***");
+  head.meta("twitter:site", "***REMOVED***");
+  head.meta("theme-color", "#ff581a");
 
-    let descriptionText = post.description;
-    const descriptionStatus = [];
+  let descriptionText = post.description;
+  const descriptionStatus = [];
 
-    let type = 'object';
+  let type = "object";
 
-    switch (post.post_hint) {
-        case 'image':
-            type = 'photo';
-            head.meta('twitter:card', 'summary_large_image');
-            head.image(post.url, post.resolution?.width, post.resolution?.height);
-            break;
-        // case 'rich:video':
-        case 'hosted:video':
-            type = 'video.other';
-            head.video(post.video_url ?? post.url, post.resolution?.width, post.resolution?.height);
-            break;
-        case 'link':
-        default: {
-            const domainHandler = getDomainHandler(post.domain);
-            if (domainHandler) {
-                type = domainHandler.type;
-                await domainHandler.handler(post, post.url, head);
-            } else if (post.media_metadata && post.media_metadata.length) {
-                head.meta('twitter:card', 'summary_large_image');
-                const amount = post.media_metadata.length;
+  switch (post.post_hint) {
+    case "image":
+      type = "photo";
+      head.meta("twitter:card", "summary_large_image");
+      head.image(post.url, post.resolution?.width, post.resolution?.height);
+      break;
+    // case 'rich:video':
+    case "hosted:video":
+      type = "video.other";
+      head.video(
+        post.video_url ?? post.url,
+        post.resolution?.width,
+        post.resolution?.height
+      );
+      break;
+    case "link":
+    default: {
+      const domainHandler = getDomainHandler(post.domain);
+      if (domainHandler) {
+        type = domainHandler.type;
+        await domainHandler.handler(post, post.url, head);
+      } else if (post.media_metadata && post.media_metadata.length) {
+        head.meta("twitter:card", "summary_large_image");
+        const amount = post.media_metadata.length;
 
-                if (amount > 1) {
-                    descriptionStatus.push(`🖼️ Gallery: ${post.media_metadata.length} Images`);
-                }
-
-                let index = 1;
-                for (const image of post.media_metadata) {
-                    head.image(image.url, image.width, image.height);
-                    if (image.caption?.length) {
-                        head.meta('twitter:image:alt', image.caption);
-                        head.meta('og:image:alt', image.caption);
-                    } else if (amount > 1) {
-                        head.meta('twitter:image:alt', `Image ${index} of ${amount}`);
-                        head.meta('og:image:alt', `Image ${index} of ${amount}`);
-                        index++;
-                    }
-                }
-            } else if (post.oembed) {
-                head.image(post.oembed.thumbnail_url);
-                descriptionText += post.oembed.title;
-            } else if (post.preview_image_url) {
-                head.image(post.preview_image_url, post.resolution?.width, post.resolution?.height);
-            } else {
-                const url = new URL(post.url);
-                if (url.pathname.endsWith('.png') || url.pathname.endsWith('.jpg') || url.pathname.endsWith('.gif')) {
-                    head.meta('twitter:card', 'summary_large_image');
-                    head.image(post.url);
-                } else if (url.pathname.endsWith('.mp4')) {
-                    head.video(post.url);
-                }
-            }
-
-            break;
+        if (amount > 1) {
+          descriptionStatus.push(
+            `🖼️ Gallery: ${post.media_metadata.length} Images`
+          );
         }
+
+        let index = 1;
+        for (const image of post.media_metadata) {
+          head.image(image.url, image.width, image.height);
+          if (image.caption?.length) {
+            head.meta("twitter:image:alt", image.caption);
+            head.meta("og:image:alt", image.caption);
+          } else if (amount > 1) {
+            head.meta("twitter:image:alt", `Image ${index} of ${amount}`);
+            head.meta("og:image:alt", `Image ${index} of ${amount}`);
+            index++;
+          }
+        }
+      } else if (post.oembed) {
+        head.image(post.oembed.thumbnail_url);
+        descriptionText += post.oembed.title;
+      } else if (post.preview_image_url) {
+        head.image(
+          post.preview_image_url,
+          post.resolution?.width,
+          post.resolution?.height
+        );
+      } else {
+        const url = new URL(post.url);
+        if (
+          url.pathname.endsWith(".png") ||
+          url.pathname.endsWith(".jpg") ||
+          url.pathname.endsWith(".gif")
+        ) {
+          head.meta("twitter:card", "summary_large_image");
+          head.image(post.url);
+        } else if (url.pathname.endsWith(".mp4")) {
+          head.video(post.url);
+        }
+      }
+
+      break;
     }
+  }
 
-    head.meta('og:type', type);
+  head.meta("og:type", type);
 
-    // Set the description based on the post content and status
-    const description = (descriptionStatus.join(' ') + '\n\n' + descriptionText).trim();
-    if (description.length) {
-        head.meta('og:description', description);
-        head.meta('twitter:description', description);
-    }
+  // Set the description based on the post content and status
+  const description = (
+    descriptionStatus.join(" ") +
+    "\n\n" +
+    descriptionText
+  ).trim();
+  if (description.length) {
+    head.meta("og:description", description);
+    head.meta("twitter:description", description);
+  }
 
-    return html;
+  return html;
 }
--- src/worker.ts
+++ src/worker.ts
@@ -1,165 +1,181 @@
-import { IRequest, Router, html as HtmlResponse } from 'itty-router';
-import { postToHtml } from './reddit/compile';
-import { parseRedditPost } from './reddit/parse';
-import { RedditListingResponse, RedditPost } from './reddit/types';
-import { Sentry } from '@borderless/worker-sentry';
-import { HTMLElement } from 'node-html-parser';
-import { CACHE_CONFIG } from './cache';
+import { IRequest, Router, html as HtmlResponse } from "itty-router";
+import { postToHtml } from "./reddit/compile";
+import { parseRedditPost } from "./reddit/parse";
+import { RedditListingResponse, RedditPost } from "./reddit/types";
+import { Sentry } from "@borderless/worker-sentry";
+import { HTMLElement } from "node-html-parser";
+import { CACHE_CONFIG } from "./cache";
 
 const sentry = new Sentry({
-    dsn: SENTRY_ENDPOINT,
+  dsn: "***REMOVED***",
 
-    // Performance Monitoring
-    // tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Performance Monitoring
+  // tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
 });
 
-const REDDIT_BASE_URL = 'https://www.reddit.com';
-const CUSTOM_DOMAIN = 'rxddit.com';
-// const WORKER_DOMAIN = 'vxreddit.minn.workers.dev';
+const REDDIT_BASE_URL = "https://www.reddit.com";
+//const CUSTOM_DOMAIN = "rxddit.com";
+const CUSTOM_DOMAIN = "***REMOVED***";
 
 const FETCH_HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0',
-    'Accept': 'application/json',
-    'Accept-Language': 'en-US,en;q=0.5',
+  "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0",
+  Accept: "application/json",
+  "Accept-Language": "en-US,en;q=0.5",
 };
 
 const RESPONSE_HEADERS = {
-    'Content-Type': 'text/html; charset=UTF-8',
-    'Cache-Control': 'public, max-age=86400'
+  "Content-Type": "text/html; charset=UTF-8",
+  "Cache-Control": "public, max-age=86400",
 };
 
 class ResponseError extends Error {
-    constructor(public status: number, public error: string) {
-        super(`${status}: ${error}`);
-    }
+  constructor(public status: number, public error: string) {
+    super(`${status}: ${error}`);
+  }
 }
 
-async function get_post(id: string, subreddit?: string, slug?: string): Promise<RedditPost> {
-    let url = REDDIT_BASE_URL;
-    if (subreddit && slug) {
-        url += `/r/${subreddit}/comments/${id}/${slug}.json`;
-    } else {
-        url += `/${id}.json`;
-    }
+async function get_post(
+  id: string,
+  subreddit?: string,
+  slug?: string
+): Promise<RedditPost> {
+  let url = REDDIT_BASE_URL;
+  if (subreddit && slug) {
+    url += `/r/${subreddit}/comments/${id}/${slug}.json`;
+  } else {
+    url += `/${id}.json`;
+  }
 
-    return await fetch(url, { headers: FETCH_HEADERS, ...CACHE_CONFIG })
-        .then((r) => r.ok ? r.json<RedditListingResponse[]>() : Promise.reject(new ResponseError(r.status, r.statusText)))
-        .then(([json]) => parseRedditPost(json));
+  return await fetch(url, { headers: FETCH_HEADERS, ...CACHE_CONFIG })
+    .then((r) =>
+      r.ok
+        ? r.json<RedditListingResponse[]>()
+        : Promise.reject(new ResponseError(r.status, r.statusText))
+    )
+    .then(([json]) => parseRedditPost(json));
 }
 
 function isBot({ headers }: IRequest): boolean {
-    return headers.get('User-Agent')?.toLowerCase()?.includes('bot') ?? false;
+  return headers.get("User-Agent")?.toLowerCase()?.includes("bot") ?? false;
 }
 
 function getOriginalUrl(url: string) {
-    const location = new URL(url);
+  const location = new URL(url);
 
-    if (location.hostname.endsWith(CUSTOM_DOMAIN)) {
-        location.hostname = location.hostname.replace(CUSTOM_DOMAIN, 'reddit.com');
-    } else {
-        location.hostname = 'reddit.com';
-    }
+  if (location.hostname.endsWith(CUSTOM_DOMAIN)) {
+    location.hostname = location.hostname.replace(CUSTOM_DOMAIN, "reddit.com");
+  } else {
+    location.hostname = "reddit.com";
+  }
 
-    location.protocol = 'https:';
-    location.port = '';
+  location.protocol = "https:";
+  location.port = "";
 
-    return location.toString();
+  return location.toString();
 }
 
 function fallbackRedirect(req: IRequest) {
-    const url = getOriginalUrl(req.url);
+  const url = getOriginalUrl(req.url);
 
-    return HtmlResponse(`<head><meta http-equiv="Refresh" content="0; URL=${url.replaceAll('"', '\\"')}" /></head>`, {
-        headers: { Location: url }, status: 302
-    });
+  return HtmlResponse(
+    `<head><meta http-equiv="Refresh" content="0; URL=${url.replaceAll(
+      '"',
+      '\\"'
+    )}" /></head>`,
+    {
+      headers: { Location: url },
+      status: 302,
+    }
+  );
 }
 
 const router = Router();
 
 async function handlePost(request: IRequest) {
-    const { params, url } = request;
-    const { name, id, slug } = params;
-    const originalLink = getOriginalUrl(url);
-    const bot = isBot(request);
+  const { params, url } = request;
+  const { name, id, slug } = params;
+  const originalLink = getOriginalUrl(url);
+  const bot = isBot(request);
 
-    const headers: HeadersInit = { ...RESPONSE_HEADERS };
-    const { protocol } = new URL(url);
-    if (protocol === 'https:') {
-        headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains; preload';
+  const headers: HeadersInit = { ...RESPONSE_HEADERS };
+  const { protocol } = new URL(url);
+  if (protocol === "https:") {
+    headers["Strict-Transport-Security"] =
+      "max-age=31536000; includeSubDomains; preload";
+  }
+
+  if (!bot) {
+    // forcing redirect for browsers
+    headers["Location"] = originalLink;
+  }
+
+  try {
+    const post = await get_post(id, name, slug);
+    const html = await postToHtml(post);
+
+    html
+      .querySelector("head")
+      ?.appendChild(
+        new HTMLElement("meta", {})
+          .setAttribute("http-equiv", "Refresh")
+          .setAttribute("content", `0; URL=${originalLink}`)
+      );
+
+    return new Response(html.toString(), {
+      headers,
+      status: bot ? 200 : 302,
+    });
+  } catch (err) {
+    const { status } = err as ResponseError;
+    if (status === 404) {
+      return new Response("Post not found", {
+        headers,
+        status,
+      });
+    } else {
+      throw err;
     }
-
-    if (!bot) { // forcing redirect for browsers
-        headers['Location'] = originalLink;
-    }
-
-    try {
-        const post = await get_post(id, name, slug);
-        const html = await postToHtml(post);
-        
-        html.querySelector('head')?.appendChild(new HTMLElement('meta', {})
-            .setAttribute('http-equiv', 'Refresh')
-            .setAttribute('content', `0; URL=${originalLink}`)
-        );
-
-        return new Response(html.toString(), {
-            headers,
-            status: bot ? 200 : 302
-        });
-    } catch (err) {
-        const { status } = err as ResponseError;
-        if (status === 404) {
-            return new Response('Post not found', {
-                headers,
-                status
-            });
-        } else {
-            throw err;
-        }
-    }
+  }
 }
 
-const ROBOTS_TXT = () => new Response('User-agent: *\nDisallow: /', { headers: { 'Content-Type': 'text/plain' } });
-const SECURITY_TXT = () => new Response('Contact: https://github.com/MinnDevelopment/fxreddit/issues/new', { headers: { 'Content-Type': 'text/plain' } });
-const NOT_FOUND = () => new Response('Not Found', { status: 404 });
+const ROBOTS_TXT = () =>
+  new Response("User-agent: *\nDisallow: /", {
+    headers: { "Content-Type": "text/plain" },
+  });
+const SECURITY_TXT = () =>
+  new Response(
+    "Contact: https://github.com/MinnDevelopment/fxreddit/issues/new",
+    { headers: { "Content-Type": "text/plain" } }
+  );
+const NOT_FOUND = () => new Response("Not Found", { status: 404 });
 
 router
-    // Redirect all browser usage
-    // .all('*', (req) => redirectBrowser(req))
-    // Block all robots / crawlers
-    .get('/robots.txt', ROBOTS_TXT)
-    .get('/security.txt', SECURITY_TXT)
-    .get('/*.ico', NOT_FOUND)
-    .get('/*.txt', NOT_FOUND)
-    .get('/*.xml', NOT_FOUND)
-    // Otherwise, if its a bot we respond with a meta tag page
-    .get('/r/:name/comments/:id/:slug?', handlePost)
-    .get('/:id', handlePost)
-    // On missing routes we simply redirect
-    .all('*', fallbackRedirect);
+  // Redirect all browser usage
+  // .all('*', (req) => redirectBrowser(req))
+  // Block all robots / crawlers
+  .get("/robots.txt", ROBOTS_TXT)
+  .get("/security.txt", SECURITY_TXT)
+  .get("/*.ico", NOT_FOUND)
+  .get("/*.txt", NOT_FOUND)
+  .get("/*.xml", NOT_FOUND)
+  // Otherwise, if its a bot we respond with a meta tag page
+  .get("/r/:name/comments/:id/:slug?", handlePost)
+  .get("/:id", handlePost)
+  // On missing routes we simply redirect
+  .all("*", fallbackRedirect);
 
-addEventListener('fetch', (event) => {
-    event.respondWith(router.handle(event.request).catch((err) => {
-        // Extend the event lifetime until the response from Sentry has resolved.
-        // Docs: https://developers.cloudflare.com/workers/runtime-apis/fetch-event#methods
-        console.error(err);
-        event.waitUntil(
-            // Sends a request to Sentry and returns the response promise.
-            sentry.captureException(err, {
-                tags: {
-                    level: 'handler',
-                },
-                request: {
-                    url: event.request.url,
-                    method: event.request.method,
-                    headers: Object.fromEntries(event.request.headers.entries()),
-                },
-                user: {
-                    ip: event.request.headers.get('cf-connecting-ip') ?? undefined,
-                },
-            }).catch(console.error)
-        );
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    router.handle(event.request).catch((err) => {
+      // Extend the event lifetime until the response from Sentry has resolved.
+      // Docs: https://developers.cloudflare.com/workers/runtime-apis/fetch-event#methods
+      console.error(err);
 
-        // Respond to the original request while the error is being logged (above).
-        return new Response(err.message || 'Internal Server Error', { status: 500 });
-    }));
+      // Respond to the original request while the error is being logged (above).
+      return new Response(err.message || "Internal Server Error", {
+        status: 500,
+      });
+    })
+  );
 });
